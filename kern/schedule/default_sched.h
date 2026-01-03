48a7eebe3902cd47de35cb132200225c93b7aa1b
Elomeme <2010549563@qq.com>
2026-01-03 16:39:41 +0800

    lab6

---
+++ kern/schedule/default_sched.h
@@ -0,0 +1,10 @@
+#ifndef __KERN_SCHEDULE_SCHED_RR_H__
+#define __KERN_SCHEDULE_SCHED_RR_H__
+
+#include <sched.h>
+
+extern struct sched_class default_sched_class;//RR
+extern struct sched_class stride_sched_class;//stride
+
+#endif /* !__KERN_SCHEDULE_SCHED_RR_H__ */
+
